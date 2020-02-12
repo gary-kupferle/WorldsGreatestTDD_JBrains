f2b5513388cdea24877e56e72516a2566699b6fa
Gary Kupferle <gary.kupferle+github@gmail.com>
2020-02-12 11:45:22 -0600

    Commit and push failing test for purpose of testing Git/GitHub setup.

---
+++ re-intro/src/test/Java/jbrains.wgtdd/FirstTest.java
@@ -0,0 +1,11 @@
+package jbrains.wgtdd;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+    @Test
+    public void simpleTest() {
+        assertTrue(false);
+    }
+}
